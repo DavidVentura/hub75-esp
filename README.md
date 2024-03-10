19485d9a2c23bf7adaf2ed2b0b63baa959301eac
David Ventura <davidventura27@gmail.com>
2024-03-10 19:14:50 +0100

    optimize addr pin placement

--- README.md
+++ README.md
@@ -7,17 +7,33 @@ As these panels are RGB111, displaying more than 1-bit color can be done by iter
 On these panels, you need to provide 2 rows of data at once: x and x+16; both halves of the panel are filled simultaneously.
 
 ## Image format
-The image format I came up with is having the pixel for both rows packed into the same byte: `[r1_g1b1r2_g2b2]` where `_` are two ignored bytes.
+The image format I came up with is having the pixel for both rows packed into the same byte: `[r1_g1b1r2_g2b2]` where `_` are two ignored bits.
 
 The ignored byte is cleverly placed in that position to reduce the amount of bit-shifting necessary, and it constrains the relationship between usable pins.
 
-At 64x32, each RGB image takes 1KiB per bit of depth -- a normal image with 8bit depth takes 8KiB. Overhead is 25% (6/8 bits are used).
+At 64x32, each RGB image takes 1KiB per bit of depth -- a 6-bit-depth image uses 6KiB. Overhead is 25% (6/8 bits are used).
+
+The screens are 16bit -- RGB565 so going beyond 6 bit depth doesn't make sense.
 
 ## Speed
 
-Each 'frame' can be rendered by bit-banging the protocol in ~150us; but that has to be multiplied with the bit depth (6 => 2^6 => 64) and the brightness PWM factor (currently disabled) which makes each full frame take ~9.5ms.
+Each 'frame' can be rendered by bit-banging the protocol in ~120us; but that has to be multiplied with the bit depth (6 => 2^6 => 64) and the brightness PWM factor (currently disabled) which makes each full frame take ~7.9ms.
 
+
+Calculation is that this runs at 8.2MHz and that's probably the upper limit 
+
+64 col * 16 row * 64 (2^6 bit depth) = 65536 updates ; 7.9ms =>  126fps
+
+## Limitations
+
+- Only pins 0..=31 can be used for ADDR and DATA; as they are part of the port 0 and can be changed in 1 write
+- This is a software implementation - each 6-bit frame takes 7.9ms to render and it must be called roughly once every 20ms to have no flicker.
+	- ~Half of one core will be consumed by this
+	- Using 5 bit color will bring it down to ~4ms, making it 25% of one core
+- The relationship between the used pins is constrained as an optimization
+	- You need to use 3 ~contiguous pins (at most a gap of 1; for me: 4, 5, 6) for R1G1B1
+	- You need to use 3 ~contiguous pins (at most a gap of 1; for me: 18, 19, 21) for R2G2B2
+	- You need to use 4 contiguous pins (for me: 12, 13, 14, 15) for ABCD
 
 ## Other
-
 There's a script (`script.py`) which can convert a 64x32 PNG into a rust array, which you can copy-paste into the source.
--- src/main.rs
+++ src/main.rs
@@ -27,13 +27,18 @@ fn main() {
     let r2 = PinDriver::output(p.pins.gpio18).unwrap();
     let g2 = PinDriver::output(p.pins.gpio19).unwrap();
     let b2 = PinDriver::output(p.pins.gpio21).unwrap();
-    let a = PinDriver::output(p.pins.gpio23).unwrap();
+
+    let a = PinDriver::output(p.pins.gpio12).unwrap();
     let b = PinDriver::output(p.pins.gpio13).unwrap();
     let c = PinDriver::output(p.pins.gpio14).unwrap();
-    let d = PinDriver::output(p.pins.gpio27).unwrap();
-    let _clk = PinDriver::output(p.pins.gpio15).unwrap();
-    let mut lat = PinDriver::output(p.pins.gpio25).unwrap();
-    let mut oe = PinDriver::output(p.pins.gpio26).unwrap();
+    let d = PinDriver::output(p.pins.gpio15).unwrap();
+
+    let clk = PinDriver::output(p.pins.gpio25).unwrap();
+    let lat = PinDriver::output(p.pins.gpio26).unwrap();
+    let mut oe = PinDriver::output(p.pins.gpio27).unwrap();
+
+    let lat_pin = lat.pin() as u8;
+    let oe_pin = oe.pin() as u8;
 
     let rgb1_mask: u32 = (1 << r1.pin()) | (1 << g1.pin()) | (1 << b1.pin());
     let rgb2_mask: u32 = (1 << r2.pin()) | (1 << g2.pin()) | (1 << b2.pin());
@@ -42,111 +47,113 @@ fn main() {
 
     let addrmask: u32 = (1 << a.pin()) | (1 << b.pin()) | (1 << c.pin()) | (1 << d.pin());
 
+    let clkpin = clk.pin() as u8;
+
     loop {
         for frame in frames.iter() {
-            for rep in 0..5 {
+            // make frames last longer
+            for _ in 0..5 {
                 let _start = Instant::now();
                 let mut count = 0;
-                let mut incount: u64 = 0;
                 // enable output
                 oe.set_low().unwrap();
                 // silly pwm
-                for brightness in [u32::MAX] {
-                    let mut bit_nr = frame.len();
-                    for data in frame {
-                        count += 1;
-                        // this is a simple binary coded modulation which gives more time on for more
-                        // significant bits; explanation in littel-endian ([n, n-1, .., 1, 0])
-                        //
-                        // to modulate different colors on RGB111,
-                        // bit n     gets 2^(n  ) frames ON/OFF
-                        // bit (n-1) gets 2^(n-2) frames ON/OFF
-                        // ...
-                        // bit (0)   gets 2^(0  ) frames ON/OFF
-                        //
-                        // this makes the MSB have a larger impact in perception, simulating more color
-                        // resolution by utilizing the time factor instead of variable brightness
-                        let tot_frames = 1 << (bit_nr - 1);
-                        for _ in 0..tot_frames {
-                            for i in 0..data.len() {
-                                let row = data[i];
-                                oe.set_low().unwrap();
-                                for x in 0..row.len() {
-                                    incount += 1;
-                                    let element = row[x] as u32;
-                                    let rgb1 = element & 0b1101_0000;
-                                    let rgb2 = element & 0b0000_1011;
+                let mut bit_nr = frame.len();
+                for data in frame {
+                    count += 1;
+                    // this is a simple binary coded modulation which gives more time on for more
+                    // significant bits; explanation in littel-endian ([n, n-1, .., 1, 0])
+                    //
+                    // to modulate different colors on RGB111,
+                    // bit n     gets 2^(n  ) frames ON/OFF
+                    // bit (n-1) gets 2^(n-2) frames ON/OFF
+                    // ...
+                    // bit (0)   gets 2^(0  ) frames ON/OFF
+                    //
+                    // this makes the MSB have a larger impact in perception, simulating more color
+                    // resolution by utilizing the time factor instead of variable brightness
+                    let tot_frames = 1 << (bit_nr - 1);
+                    for _ in 0..tot_frames {
+                        for i in 0..data.len() {
+                            let row = data[i];
+                            //oe.set_low().unwrap();
+                            fast_pin_down(oe_pin);
+                            for x in 0..row.len() {
+                                // incount += 1;
+                                let element = row[x] as u32;
+                                let rgb1 = element & 0b1101_0000;
+                                let rgb2 = element & 0b0000_1011;
 
-                                    let pixdata: u32 = rgb1 >> 2;
-                                    let pixdata = pixdata | (rgb2 << 18);
-                                    // this & rgb_mask is safety -- could be removed
-                                    // let pixdata = pixdata & rgb_mask;
-                                    // this & rgb_mask is _not_ safety; completely necessary
-                                    let notpixdata: u32 = (!pixdata) & rgb_mask;
-                                    let pixdata = pixdata | (1 << 15); // clk
+                                let pixdata: u32 = rgb1 >> 2;
+                                let pixdata = pixdata | (rgb2 << 18);
+                                // this & rgb_mask is safety -- could be removed
+                                // let pixdata = pixdata & rgb_mask;
+                                // this & rgb_mask is _not_ safety; completely necessary
+                                let notpixdata: u32 = (!pixdata) & rgb_mask;
+                                let pixdata = pixdata | (1 << clkpin); // clk
 
-                                    unsafe {
-                                        // the data is valid when clock goes _up_
-                                        core::ptr::write_volatile(
-                                            GPIO_OUT_W1TC_REG as *mut _,
-                                            notpixdata | (1 << 15),
-                                        );
-                                        // pix data + clk
-                                        core::ptr::write_volatile(
-                                            GPIO_OUT_W1TS_REG as *mut _,
-                                            pixdata,
-                                        );
-
-                                        // could now bring clock _down_ to make sure changes are not valid
-                                        // but it is not necessary - when we call the first write_volatile
-                                        // the clock goes down
-                                        // core::ptr::write_volatile(GPIO_OUT_W1TC_REG as *mut _, 1 << 15);
-                                    }
-                                }
-                                oe.set_high().unwrap();
-                                //Ets::delay_us(2);
-                                lat.set_low().unwrap();
-                                //Ets::delay_us(2);
-                                lat.set_high().unwrap();
-                                // Select row
-
-                                // count < 16; LUT addrdata
-                                // LUT is slower
-                                let ba = (i & 1) as u32;
-                                let bb = (i & 2) as u32;
-                                let bc = (i & 4) as u32;
-                                let bd = (i & 8) as u32;
-                                let addrdata: u32 =
-                                    ((ba << 23) | (bb << 12) | (bc << 12) | (bd << 24)) as u32;
-                                let not_addrdata: u32 = !addrdata & addrmask;
                                 unsafe {
+                                    // the data is valid when clock goes _up_
                                     core::ptr::write_volatile(
                                         GPIO_OUT_W1TC_REG as *mut _,
-                                        not_addrdata,
+                                        notpixdata | (1 << clkpin),
                                     );
-                                    core::ptr::write_volatile(
-                                        GPIO_OUT_W1TS_REG as *mut _,
-                                        addrdata,
-                                    );
-                                }
+                                    // pix data + clk
+                                    core::ptr::write_volatile(GPIO_OUT_W1TS_REG as *mut _, pixdata);
 
-                                //oe.set_low().unwrap();
-                                //Ets::delay_us(2);
+                                    // could now bring clock _down_ to make sure changes are not valid
+                                    // but it is not necessary - when we call the first write_volatile
+                                    // the clock goes down
+                                    // core::ptr::write_volatile(GPIO_OUT_W1TC_REG as *mut _, 1 << clkpin);
+                                }
                             }
+                            fast_pin_up(oe_pin);
+                            //oe.set_high().unwrap();
+                            //Ets::delay_us(2);
+                            //lat.set_low().unwrap();
+                            fast_pin_down(lat_pin);
+                            //Ets::delay_us(2);
+                            //lat.set_high().unwrap();
+                            fast_pin_up(lat_pin);
+                            // Select row
+
+                            let addrdata: u32 = (i as u32) << 12;
+                            //println!("{addrdata:032b} {i:032b}");
+                            let not_addrdata: u32 = !addrdata & addrmask;
+                            unsafe {
+                                core::ptr::write_volatile(
+                                    GPIO_OUT_W1TC_REG as *mut _,
+                                    not_addrdata,
+                                );
+                                core::ptr::write_volatile(GPIO_OUT_W1TS_REG as *mut _, addrdata);
+                            }
+
+                            //oe.set_low().unwrap();
+                            //Ets::delay_us(2);
                         }
-                        bit_nr -= 1;
                     }
+                    bit_nr -= 1;
                 }
                 // Disable the output
                 // Prevents one row from being much brighter than the others
                 oe.set_high().unwrap();
-                println!(
-                    "Elapsed {:?}; count: {count}; incount {incount}",
-                    _start.elapsed()
-                );
+                println!("Elapsed {:?}; count: {count}", _start.elapsed());
                 // keep watchdog happy / lower brightness
-                //sleep(Duration::from_millis(4));
+                sleep(Duration::from_millis(4));
             }
         }
+    }
+}
+
+#[inline]
+fn fast_pin_up(idx: u8) {
+    unsafe {
+        core::ptr::write_volatile(GPIO_OUT_W1TS_REG as *mut _, 1 << (idx));
+    }
+}
+#[inline]
+fn fast_pin_down(idx: u8) {
+    unsafe {
+        core::ptr::write_volatile(GPIO_OUT_W1TC_REG as *mut _, 1 << idx);
     }
 }
